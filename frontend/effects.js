f15a15271d9ee38f8d1c04be6e39e4eb74300db4
Nathan Fedzen <nfedzen@hotmail.com>
2021-01-27 13:25:28 -0700

    styling changes

--- frontend/effects.js
+++ frontend/effects.js
@@ -24,15 +24,19 @@ fetch(strainURL)
       if(effectFilter === 'None'){
       const h4 = document.createElement('h4')
       
-      h4.innerHTML = `<a href='http://localhost:3001/strain.html?name=${strain.name}'> Name: ${strain.name} <br> Race:  ${strain.race} </a>`
+
+      h4.innerHTML = `<a style='align-content: center;' 
+      href='http://localhost:3001/strain.html?name=${strain.name}'> Name: ${strain.name} || Race: ${strain.race}</a>`
 
       savedStrains.append(h4)
       }else if(effectFilter === strain.race){
         const h4 = document.createElement('h4')
+        
       
-      h4.innerHTML = `<a href='http://localhost:3001/strain.html?name=${strain.name}'> Name: ${strain.name} <br> Race:  ${strain.race} </a>`
-
-      savedStrains.append(h4)
+        h4.innerHTML = `<a style='align-content: center;' 
+        href='http://localhost:3001/strain.html?name=${strain.name}'> Name: ${strain.name} || Race: ${strain.race}</a>`
+        
+        savedStrains.append(h4)
       }
     })
   })
